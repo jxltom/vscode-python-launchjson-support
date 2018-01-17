a1ec5be0c96eac83b100c662999069eacccbc157
Junxiao Liu <jxltom@gmail.com>
2018-01-17 13:16:29 +0800

    Add universe environment variable in settings.json

--- custom_package/main.py
+++ custom_package/main.py
@@ -2,4 +2,5 @@ import os
 import custom_package
 
 print('custom package is imported which means PYTHONPATH as env is set appropriately')
-print(os.environ.get('MYENV'))
+print(os.environ.get('ENV'))
+print(os.environ.get('ENVFILE'))
