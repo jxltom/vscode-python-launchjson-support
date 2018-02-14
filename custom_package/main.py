6ec66c7cb0fd6b6a5f196dd8aa695ce360674ffb
Junxiao Liu <jxltom@gmail.com>
2018-02-14 16:10:49 +0800

    Update to latest usage

--- custom_package/main.py
+++ custom_package/main.py
@@ -2,5 +2,4 @@ import os
 import custom_package
 
 print('custom package is imported which means PYTHONPATH as env is set appropriately')
-print(os.environ.get('ENV'))
 print(os.environ.get('ENVFILE'))
